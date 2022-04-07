82c5ab7579fe61ed617e4b3d17241c9549d305a5
gaozheng <2272700510@qq.com>
2022-04-07 10:22:51 +0800

    引入vant 组件库

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,20 @@
   <div id="app">
     <router-view />
     <h1>黑马头条</h1>
+    <div>
+      <i class="iconfont icon-dianzan2"></i>
+    </div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <van-cell-group inset>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
+    <van-icon name="chat-o" />
+    <van-icon name="https://b.yzcdn.cn/vant/icon-demo-1126.png" />
   </div>
 </template>
 <script>
